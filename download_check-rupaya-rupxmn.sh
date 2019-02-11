be223785ccb308d88ba52aa762b5539b80964039
Brian Bearden <35510406+BlockchainBrain@users.noreply.github.com>
2019-02-11 17:09:54 -0600

    Delete download_check-rupaya-rupxmn.sh

--- download_check-rupaya-rupxmn.sh
+++
@@ -1,8 +0,0 @@
-#Download and run a bash script that will check for the Rupaya daemon and restart it if it is not running.
-echo Downloading the bash script
-wget https://github.com/BlockchainBrain/Rupaya_scripts/blob/master/check-rupaya-rupxmn.sh
-wait
-echo Updating the file to grant execution permissions
-chmod +x check-rupaya-root.sh
-echo Running the bash script
-bash check-rupaya-root.sh
