831ba05792a6505637f26491328d51146ab16f01
Brian Bearden <35510406+BlockchainBrain@users.noreply.github.com>
2019-02-12 14:41:05 -0600

    Update profile-scripts-rupxmn-20MNs.sh
    Making the commands bold

--- profile-scripts-rupxmn-20MNs.sh
+++ profile-scripts-rupxmn-20MNs.sh
@@ -7,26 +7,28 @@
 # The TXHash for each MN can be found in the masternode.conf file on the PC running the Cold Wallet.
 # The logall and log1 commands will not work unless the vars.bash file is filled in with the correct TXHash for each MN.
 #
+bold=$(tput bold)
+normal=$(tput sgr0)
 {
 echo "Downloading the vars.bash file"
 echo "You will need to edit this file manually to add in your MN TXHashes."
-wget https://raw.githubusercontent.com/BlockchainBrain/Rupaya_scripts/master/vars.bash >/dev/null
+wget https://raw.githubusercontent.com/BlockchainBrain/Rupaya_scripts/master/vars.bash
 wait
-sudo mv vars.bash /home/rupxmn/vars.bash >/dev/null
+sudo mv vars.bash /home/rupxmn/vars.bash
 echo "Downloading the file named profile-scripts-rupxmn-20MNs.profile and overwriting the /home/rupxmn/.profile"
-wget https://raw.githubusercontent.com/BlockchainBrain/Rupaya_scripts/master/profile-scripts-rupxmn-20MNs.profile >/dev/null
+wget https://raw.githubusercontent.com/BlockchainBrain/Rupaya_scripts/master/profile-scripts-rupxmn-20MNs.profile
 wait
-sudo rm /home/rupxmn/.profile >/dev/null
+sudo rm /home/rupxmn/.profile
 sudo mv profile-scripts-rupxmn-20MNs.profile /home/rupxmn/.profile
 echo ""
 echo "The profile script download is complete"
 echo "Run the following command to edit the varsh.bash file so you can add in your TXHashes:"
 echo "The TXHash for each MN can be found in the masternode.conf file on the PC running the Cold Wallet."
-echo "nano /home/rupxmn/vars.bash"
+echo "${bold}nano /home/rupxmn/vars.bash${normal}"
 echo ""
-echo "To get a list of commands that you can now run, enter the command: list"
+echo "To view a list of the ommands that you can now run, enter the command: ${bold}list${normal}"
 echo ""
 echo "You must run the following command to enable the profile scripts:"
-echo "source /home/rupxmn/.profile"
+echo "${bold}source /home/rupxmn/.profile${normal}"
 echo ""
 }
