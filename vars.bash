cd16b192051da3cb60ed54609703b19cb44ae272
Brian Bearden <35510406+BlockchainBrain@users.noreply.github.com>
2019-02-12 12:52:56 -0600

    Create vars.bash
    This file will be used to store all of the IP address and TXHash variables that are used in the profile scripts

---
+++ vars.bash
@@ -0,0 +1,87 @@
+#This file will be used to store all of the IP address and TXHash variables that are used in the profile scripts
+#This file will be stored in /home/rupxmn/vars.bash
+#Transaction Hashes
+H1=
+H2=
+H3=
+H4=
+H5=
+H6=
+H7=
+H8=
+H9=
+H10=
+H11=
+H12=
+H13=
+H14=
+H15=
+H16=
+H17=
+H18=
+H19=
+H20=
+#
+export H1
+export H2
+export H3
+export H4
+export H5
+export H6
+export H7
+export H8
+export H9
+export H10
+export H11
+export H12
+export H13
+export H14
+export H15
+export H16
+export H17
+export H18
+export H19
+export H20
+
+#MN IP Addresses
+IP1=
+IP2=
+IP3=
+IP4=
+IP5=
+IP6=
+IP7=
+IP8=
+IP9=
+IP10=
+IP11=
+IP12=
+IP13=
+IP14=
+IP15=
+IP16=
+IP17=
+IP18=
+IP19=
+IP20=
+
+export IP1
+export IP2
+export IP3
+export IP4
+export IP5
+export IP6
+export IP7
+export IP8
+export IP9
+export IP10
+export IP11
+export IP12
+export IP13
+export IP14
+export IP15
+export IP16
+export IP17
+export IP18
+export IP19
+export IP20
