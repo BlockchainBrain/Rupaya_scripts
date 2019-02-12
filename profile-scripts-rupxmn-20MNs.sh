962d3be8f03643dda5a4cef34a92e720f6a638e7
Brian Bearden <35510406+BlockchainBrain@users.noreply.github.com>
2019-02-12 14:09:34 -0600

    profile-scripts-rupxmn-20MNs.sh
    Updated the steps to reflect that the log1 and logall commands won't work until the TXHashes are updated in the vars.bash file.

--- profile-scripts-rupxmn-20MNs.sh
+++ profile-scripts-rupxmn-20MNs.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+#
 # This script will download the files named vars.bash and profile-scripts-rupxmn-20MNs.profile 
 # and will then overwrite the existing profile in /home/rupxmn/.profile
+#
+# The TXHash for each MN can be found in the masternode.conf file on the PC running the Cold Wallet.
+# The logall and log1 commands will not work unless the vars.bash file is filled in with the correct TXHash for each MN.
+#
 echo Downloading the vars.bash file
-echo You will need to edit this file manually to add in your MN public IP addresses and TXHashes.
+echo You will need to edit this file manually to add in your MN TXHashes.
 wget https://raw.githubusercontent.com/BlockchainBrain/Rupaya_scripts/master/vars.bash
 wait
 sudo mv vars.bash /home/rupxmn/vars.bash
@@ -11,8 +17,9 @@ wait
 sudo rm /home/rupxmn/.profile
 sudo cp profile-scripts-rupxmn-20MNs.profile /home/rupxmn/.profile
 echo Running the command: source /home/rupxmn/.profile
-echo You will need to run this command again after you update the vars.bash file with your IP addresses and TXHashes
+echo You will need to run this command again after you update the vars.bash file with your TXHashes
 source /home/rupxmn/.profile
 echo the profile script download is complete
-echo Run the following command to edit the varsh.bash file so you can add in your IP addresses and TXHashes:
+echo Run the following command to edit the varsh.bash file so you can add in your TXHashes:
+echo The TXHash for each MN can be found in the masternode.conf file on the PC running the Cold Wallet.
 echo nano /home/rupxmn/vars.bash
